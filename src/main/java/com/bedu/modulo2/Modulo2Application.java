5ef5f6491e3401488ec631bcb1a1b26afd79b65b
Edgar Casanova <edgar.cardoz@gmail.com>
2023-04-25 18:31:11 -0600

    first commit

---
+++ src/main/java/com/bedu/modulo2/Modulo2Application.java
@@ -0,0 +1,13 @@
+package com.bedu.modulo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Modulo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Modulo2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/bedu/modulo2/Modulo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bedu.modulo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Modulo2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
