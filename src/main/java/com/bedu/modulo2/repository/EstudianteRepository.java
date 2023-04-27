62f2355ba6e67b8e8df8e5ebb38cf9068d2a5fa6
Daniel Maldonado <daniel_dlcmaldonado@hotmail.com>
2023-04-26 22:23:53 -0600

    Se agrega Estudiante y EstudianteRepository

---
+++ src/main/java/com/bedu/modulo2/repository/EstudianteRepository.java
@@ -0,0 +1,7 @@
+package com.bedu.modulo2.repository;
+
+import com.bedu.modulo2.model.Estudiante;
+import org.springframework.data.repository.CrudRepository;
+
+public interface EstudianteRepository extends CrudRepository<Estudiante, Long> {
+}
